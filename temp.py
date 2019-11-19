ee0175c76379fc5569b3d01a79d30d61a06706aa
TahaAlTamimi <altamimitmo@gmail.com>
2019-11-19 15:32:18 +0200

    exercise

---
+++ temp.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#name=input('tell me')
+#age=input('your age:')
+#print('hello',name,'your age',age)
+#raduis=input('raduis please:')
+#results=3.14*int(raduis)**2
+#print('the',raduis,'area equal',results)
+
+#st='heello'
+#print(st)#
+#print('hello', 'world')
+#X = range(6)
+#print((X))
+
+
+#print('Hello',sep='\n')
+#print('    Hello',sep='\n')
+#print('        Hello')
+
+#print('Orange Academy\n'*20)
+"""
+x=float(1)
+y=float(2.8)
+z=float("3")
+w=float("4.2")
+print(x,y,z,w,sep='10')
+"""
+"""
+x=int(input('num1'))
+y=int(input('num2'))
+print('num one is: ',x)
+print('num two is: ',y)
+print('the product is: ',x+y)
+"""
+"""
+a=int(input('num1'))
+b=int(input('num2'))
+c=int(input('num3'))
+d=int(input('num4'))
+e=int(input('num5'))
+print('the input numbers are:',a,b,c,d,e )
+results=(a+b+c+d+e)/5
+print('the avarage:',results)
+"""
+"""
+x=1
+y=2.8
+z=1j
+o='Orange'
+A=True
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(o))
+print(type(A))
+"""
+"""
+x,y="Orange",1
+X1,Y1=100,-10
+print(x)
+print(y)
+print(X1)
+print(Y1)
+"""
+"""
+x=input('input your name:')
+y=int(input('your age:'))
+age=100-y
+year=2019+age
+print(year)
+"""
+"""
+x=int(input('base'))
+y=int(input('height'))
+print(.5*x*y)
+"""
+"""
+x=11
+y=3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(abs(x*-1))
+print(int(x))
+print(float(x))
+print(divmod(x,y))
+print(pow(x,y))
+print(x**y)
+print(x>y)
+print(x==y)
+print(x!=y)
+"""
+for var in list(range(5)):
+  x+str(var)=  input('num: ' + str(var) + ":  ")
+    
